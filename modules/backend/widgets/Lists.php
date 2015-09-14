0bcd51cf380701a0a98a8a9cbab40e104b810161
Aydan Coskun <aydancoskun@users.noreply.github.com>
2015-09-14 09:21:46 +0000

    composer update

--- modules/backend/widgets/Lists.php
+++ modules/backend/widgets/Lists.php
@@ -767,7 +767,7 @@ class Lists extends WidgetBase
                 $value = implode(', ', $record->{$columnName}->lists($column->valueFrom));
             }
             elseif ($this->isColumnRelated($column) || $this->isColumnPivot($column)) {
-                $value = $record->{$columnName}->{$column->valueFrom};
+                $value = $record->{$columnName} ? $record->{$columnName}->{$column->valueFrom} : null;
             }
             else {
                 $value = null;
@@ -804,7 +804,7 @@ class Lists extends WidgetBase
         /*
          * Apply default value.
          */
-        if (empty($value)) {
+        if ($value === '' || $value === null) {
             $value = $column->defaults;
         }
 
